96ba4a52399852746286367c9c4cb3477e2965b1
Adrian Gonzalez <adrian.gonzalez@gmail.com>
2022-08-02 14:14:57 -0500

    removed unused code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import Header from './components/Header';
 
 function App() {
-    const name = 'Adrian';
-    const x = true;
-
     return (
         <div className='container'>
             <Header></Header>
