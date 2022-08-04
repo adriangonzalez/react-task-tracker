4e52cfd0809f2a0c02dc40d395933901465f8d76
Adrian Gonzalez <adrian.gonzalez@gmail.com>
2022-08-04 05:56:39 -0500

    implement save task

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,13 +1,27 @@
-import { Button } from './Button';
 import { useState } from 'react';
 
-const AddTask = () => {
+const AddTask = ({ onAdd }) => {
     const [text, setText] = useState('');
     const [day, setDay] = useState('');
     const [reminder, setReminder] = useState(false);
 
+    const onSubmit = (e) => {
+        e.preventDefault();
+
+        if (!text) {
+            alert('Please add task');
+            return;
+        }
+
+        onAdd({ text, day, reminder });
+
+        setText('');
+        setDay('');
+        setReminder(false);
+    };
+
     return (
-        <form className='add-form'>
+        <form className='add-form' onSubmit={onSubmit}>
             <div className='form-control'>
                 <label htmlFor=''>Task</label>
                 <input
@@ -19,17 +33,21 @@ const AddTask = () => {
             </div>
             <div className='form-control'>
                 <label htmlFor=''>Day & Time</label>
-				<input type='text' placeholder='Add Day & Time'
-					value={day}
+                <input
+                    type='text'
+                    placeholder='Add Day & Time'
+                    value={day}
                     onChange={(e) => setDay(e.target.value)}
-				/>
+                />
             </div>
             <div className='form-control form-control-check'>
                 <label htmlFor=''>Set Reminder</label>
-				<input type='checkbox'
-					value={reminder}
+                <input
+                    type='checkbox'
+                    checked={reminder}
+                    value={reminder}
                     onChange={(e) => setReminder(e.target.checked)}
-				/>
+                />
             </div>
             <input type='submit' value='Save Task' className='btn btn-block' />
         </form>
