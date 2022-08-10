4ba999d330b2f3585b422611d2dd30535f255413
Adrian Gonzalez <adrian.gonzalez@gmail.com>
2022-08-10 09:07:15 -0500

    remove unused import

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import { HashRouter, BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { HashRouter, Route, Routes } from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import { Tasks } from './components/Tasks'
